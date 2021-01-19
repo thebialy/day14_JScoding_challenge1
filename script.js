a5112ff2f3a0d19d0e32ab11a0de4907fb4484fc
thebialy <67696052+thebialy@users.noreply.github.com>
2021-01-19 16:30:13 -0600

    first coding challenege complete - reverse string

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 // Reverse a String
 
 const reverseString = (word) => {
-    const splitArr = 
+    const splitArr = word.split("");
+    const reverseArr = splitArr.reverse();
+    const reverseString = reverseArr.join("");
+
+    return reverseString
 }
+
+console.log(reverseString("love coding"));
+
+// simpler way
+// word.split("").reverse().join("")
